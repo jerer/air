e0d41b0ea4b4d4c2cc6e54af86fde39007c26ae7
Alexey Zvyagin <alexey.zvyagin@glowbyteconsulting.com>
2020-07-19 00:22:27 +0300

    w

--- dag1.py
+++ dag1.py
@@ -1,10 +1,11 @@
+import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
 
 default_args = {
     'qq': '123',
-    'start_date': datetime(2016, 1, 1),
+    'start_date': datetime.datetime(2016, 1, 1),
 }
 
 dag = DAG('my_dag', default_args=default_args)
