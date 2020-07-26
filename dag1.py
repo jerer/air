5c578cf8b342dfe08fe5d1d24a7e6ab26a67aa74
Alexey Zvyagin <alexey.zvyagin@glowbyteconsulting.com>
2020-07-26 12:17:02 +0300

    w

--- dag1.py
+++ dag1.py
@@ -7,10 +7,15 @@ from airflow.operators.bash_operator import BashOperator
 default_args = {
     'qq': '123',
     'start_date': datetime.datetime(2020, 7, 24),
-    'schedule_interval': None
+    'kubernetes_labels': {'ll100': 'qqqqqqq'}
 }
 
-dag = DAG('my_dag', default_args=default_args)
+dag = DAG(
+  'my_dag'
+, schedule_interval = None
+, default_args=default_args
+)
+
 op1 = BashOperator(task_id='dummy', bash_command='sleep 10', dag=dag)
 op2 = BashOperator(task_id='dummy100', bash_command='sleep 10', dag=dag)
 
