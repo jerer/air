1bdfe1e71f64f8bfc00c42670eb49556e6ef17d0
Alexey Zvyagin <alexey.zvyagin@glowbyteconsulting.com>
2020-07-26 17:54:15 +0300

    w

--- dag1.py
+++ dag1.py
@@ -16,7 +16,7 @@ dag = DAG(
 , default_args=default_args
 )
 
-op1 = BashOperator(task_id='dummy', bash_command='sleep 35', dag=dag,
+op1 = BashOperator(task_id='dummy', bash_command='sleep 120', dag=dag,
    executor_config={
         "KubernetesExecutor": {"request_memory": "128Mi",
                                "limit_memory": "128Mi",
